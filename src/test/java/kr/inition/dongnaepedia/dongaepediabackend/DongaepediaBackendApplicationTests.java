3036803e87e0d3b41835b2596b20a39ccde4ba10
devysk <kim206gh@naver.com>
2022-06-22 16:01:23 +0900

    second commit - project set

---
+++ src/test/java/kr/inition/dongnaepedia/dongaepediabackend/DongaepediaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.inition.dongnaepedia.dongaepediabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DongaepediaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
